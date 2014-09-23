f13a74159de77d136a5282bfabf667f8e5e49f05
Scott Dunning <sdunning@cs.csubak.edu>
2014-09-23 11:50:26 -0700

    First commit

---
+++ Address.java
@@ -0,0 +1,29 @@
+public class Address {
+
+    private int    streetNum  = 0;
+    private String streetName = "";
+    private int    apNum      = 0;
+    private int    zipCode    = 0;
+    private String city       = "";
+    private String state      = "";
+
+    public int getStreetNum()     { return streetNum; }
+    public String getStreetName() { return streetName; }
+    public int getApNum()         { return apNum; }
+    public int getZipCode()       { return zipCode; }
+    public String getCity()       { return city; }
+    public String getState()      { return state; }
+
+    public void setStreetNum(int streetNum)      { this.streetNum = streetNum; }
+    public void setStreetName(String StreetName) { this.streetName = streetName; }
+    public void setApNum(int apNum)              { this.apNum = apNum; }
+    public void setZipCode(int zipCode)          { this.zipCode = zipCode; }
+    public void setCity(String city)             { this.city = city; }
+    public void setState(String state)           { this.state = state; }
+
+    @override
+    public String toString() {
+        return String.format("%7d %20s %4d %6d %10s %10s", streetNum, streetName,
+                             apNum, zipCode, city, state);
+    }
+}
---
+++ Property.java
@@ -0,0 +1,18 @@
+public class Property {
+
+    private float price     = 0;
+    private float bathrooms = 0;
+    private int   bedrooms  = 0;
+    private int   floors    = 0;
+
+    public float getPrice()     { return price; }
+    public float getBathrooms() { return bathrooms; }
+    public int   getBedrooms()  { return bedrooms; }
+    public int   getFloors()    { return floors; }
+
+    public void setPrice(float price)          { this.price = price; }
+    public void setBathrooms(float bathrooms)  { this.bathrooms = bathrooms; }
+    public void setBedrooms(int bedrooms)      { this.bedrooms = bedrooms; }
+    public void setFloors(int floors)          { this.floors = floors; }
+
+}
